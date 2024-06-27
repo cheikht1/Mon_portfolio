172e26f0aaa86cbd965aa45a0a419ee6ea3bdf35
cheikht1 <cheikhmouhamedtidianethiam@esp.sn>
2024-06-28 01:30:52 +0200

    version 1.1

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,7 @@ provider "kubernetes" {
   config_path = "C:/Users/bmd tech/.kube/config"
 }
 
+# Déploiement de l'application web
 resource "kubernetes_manifest" "mon-portfolio-deployment" {
   manifest = yamldecode(file("../kubernetes/web_deploy.yml"))
 }
@@ -10,41 +11,24 @@ resource "kubernetes_manifest" "mon-portfolio-service" {
   manifest = yamldecode(file("../kubernetes/web_service.yml"))
 }
 
+# Déploiement de Prometheus
+resource "kubernetes_manifest" "prometheus-config" {
+  manifest = yamldecode(file("../kubernetes/prometheus/prometheus-config.yml"))
+}
 
+resource "kubernetes_manifest" "prometheus-deployment" {
+  manifest = yamldecode(file("../kubernetes/prometheus/prometheus-deployment.yml"))
+}
 
+resource "kubernetes_manifest" "prometheus-service" {
+  manifest = yamldecode(file("../kubernetes/prometheus/prometheus-service.yml"))
+}
 
+# Déploiement de Grafana
+resource "kubernetes_manifest" "grafana-deployment" {
+  manifest = yamldecode(file("../kubernetes/grafana/grafana-deployment.yml"))
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# provider "kubernetes" {
-#   config_path = "~/.kube/config"
-# }
-# resource "kubernetes_manifest" "mon-portfolio-deployment" {
-#   manifest = yamldecode(file("../kubernetes/web_deploy.yml"))
-# }
-
-# # resource "kubernetes_manifest" "deployment2" {
-# #   manifest = yamldecode(file("../kubernetes/db-deployment.yml"))
-# # }
-
-# resource "kubernetes_manifest" "mon-portfolio-service" {
-#   manifest = yamldecode(file("../kubernetes/web_service.yml"))
-# }
-# # resource "kubernetes_manifest" "web-service" {
-# #   manifest = yamldecode(file("../kubernetes/web-service.yml"))
-# # }
+resource "kubernetes_manifest" "grafana-service" {
+  manifest = yamldecode(file("../kubernetes/grafana/grafana-service.yml"))
+}
